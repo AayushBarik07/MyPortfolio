c701f95eef5c9ba7365663c9b6355912c4aff6ef
Aayush <superbsup45@gmail.com>
2025-09-01 23:50:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './components/Header';
+import HomeSection from './components/HomeSection';
+import ProjectsSection from './components/ProjectSection';
+import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col min-h-screen bg-zinc-900 text-white">
+      <Header />
+      <main className="flex-grow">
+        <HomeSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
